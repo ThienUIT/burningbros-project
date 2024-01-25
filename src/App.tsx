38f9e3120730fc60594e3bc9505bfb8fede46c1a
TranDinhThien <dinhthien2804@gmail.com>
2024-01-26 00:03:15 +0700

    Fetch product data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { useLoaderData } from "react-router-dom";
+
 function App() {
+    const product = useLoaderData();
+    console.log(product);
     return (
         <h1 className="text-5xl font-bold underline">
             Hello world!
